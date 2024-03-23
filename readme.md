f025df06b45bcaf50cab9a62690608f24a84cec8
Nabilcodes <nabil.muhammad@ui.ac.id>
2024-03-23 10:43:48 +0700

    (4) Simulation of slow request.

--- readme.md
+++ readme.md
@@ -28,3 +28,8 @@ There was some addition to the function handle_connection, which was
 
 There are a lot of repetition takes place : reading files and writing contents.
 Refactoring was made by only determining the calue fo the status line and file name in the if else block.
+
+### COMMIT 4 REFLECTION NOTES
+
+now the request handling was done using match.
+if the endpoint /sleep was called, sleep would occur before returning hello.html.
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
@@ -15,52 +17,23 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    // let buf_reader = BufReader::new(&mut stream);
-    // let http_request: Vec<_> = buf_reader
-    //     .lines()
-    //     .map(|result| result.unwrap())
-    //     .take_while(|line| !line.is_empty())
-    //     .collect();
 
-    // let status_line = "HTTP/1.1 200 OK";
-    // let contents = fs::read_to_string("hello.html").unwrap();
-    // let length = contents.len();
-
-    // let response =
-    //     format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    // stream.write_all(response.as_bytes()).unwrap();
-
-    // println!("Request: {:#?}", http_request);
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    // if request_line == "GET / HTTP/1.1" {
-    //     let status_line = "HTTP/1.1 200 OK";
-    //     let contents = fs::read_to_string("hello.html").unwrap();
-    //     let length = contents.len();
-
-    //     let response = format!(
-    //         "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-    //     );
-
-    //     stream.write_all(response.as_bytes()).unwrap();
+    // let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+    //     ("HTTP/1.1 200 OK", "hello.html")
     // } else {
-    //     let status_line = "HTTP/1.1 404 NOT FOUND";
-    //     let contents = fs::read_to_string("404.html").unwrap();
-    //     let length = contents.len();
+    //     ("HTTP/1.1 404 NOT FOUND", "404.html")
+    // };
 
-    //     let response = format!(
-    //         "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-    //     );
-
-    //     stream.write_all(response.as_bytes()).unwrap();
-    // }
-
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
@@ -70,4 +43,5 @@ fn handle_connection(mut stream: TcpStream) {
         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
+
 }
